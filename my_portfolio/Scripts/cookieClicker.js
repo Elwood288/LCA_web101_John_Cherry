db70ebe7db25b92405c4be27a2b02618f2e69aa5
Elwood288 <elwood288@gmail.com>
2019-04-08 00:54:32 -0500

    Cookie Clicker

---
+++ my_portfolio/Scripts/cookieClicker.js
@@ -0,0 +1,51 @@
+var i = 0;
+function init() {
+    document.getElementById("cookieCounter").value = "0";
+    }
+    window.onload = init;
+setTimeout()
+ function buttonClick() {
+    document.getElementById('cookieCounter').value = ++i;
+    // document.getElementById('cookieImage').style.width="25" + "vw";
+    // yield to.sleep(.500);
+    // document.getElementById('cookieImage').style.width="20" + "vw";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var counter = document.getElementById("cookieCounter"),
+//   cookieCount = 0;
+
+// function increaseCount(){
+//     cookieCount += 1;
+//     counter.innerHTML = "$" + cookieCount;
+// }
+
+// document.getElementById("cookieCounter").innerHTML = "$" + cookieCount;
